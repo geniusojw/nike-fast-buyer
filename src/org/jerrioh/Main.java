3bf5bd95940cb92a15251fe64d8bc2c8ccb4e01c
jw <jerrioh@gmail.com>
2019-01-03 00:46:22 +0900

    nike shoes

--- src/org/jerrioh/Main.java
+++ src/org/jerrioh/Main.java
@@ -1,7 +1,12 @@
 package org.jerrioh;
 
+import java.awt.AWTException;
+
+import org.jerrioh.nike.NikeShoes;
+
 public class Main {
-	public static void main(String[] ar) {
-		
+	public static void main(String[] ar) throws AWTException {
+		boolean success = new NikeShoes().buy();
+		System.out.println("result=" + success);
 	}
 }
